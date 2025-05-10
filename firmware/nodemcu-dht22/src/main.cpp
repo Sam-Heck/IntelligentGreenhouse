628771ef9cb9bdf58013189534a5375637f41e40
Sam Heck <heck.sam@gmail.com>
2025-05-09 17:19:14 -0700

    feat(firmware): add dynamic topic creation tied to device mac address to tie records to a device in db

--- firmware/nodemcu-dht22/src/main.cpp
+++ firmware/nodemcu-dht22/src/main.cpp
@@ -7,11 +7,9 @@ const char* ssid = WIFI_SSID;
 const char* password = WIFI_PASSWORD;
 const char* mqtt_server = MQTT_BROKER;
 
-// WiFi and MQTT clients
 WiFiClient espClient;
 PubSubClient client(espClient);
 
-// DHT Sensor pins and type
 #define DHTPIN1 D2
 #define DHTPIN2 D3
 #define DHTTYPE DHT22
@@ -19,8 +17,7 @@ PubSubClient client(espClient);
 DHT dht1(DHTPIN1, DHTTYPE);
 DHT dht2(DHTPIN2, DHTTYPE);
 
-// MQTT topic
-const char* topic = "greenhouse/sensors";
+String topic;
 
 void connectToWiFi() {
     Serial.print("Connecting to WiFi...");
@@ -53,6 +50,13 @@ void setup() {
 
     connectToWiFi();
 
+    // MQTT topic
+    String mac = WiFi.macAddress();
+    mac.replace(":", "");
+    mac.toLowerCase();
+    Serial.println("MAC Address: " + mac);
+
+    topic = "greenhouse/" + mac + "/sensors";
     client.setServer(mqtt_server, 1883);
     connectToMQTT();
 }
@@ -67,12 +71,6 @@ void loop() {
     float t1 = dht1.readTemperature() * 1.8 + 32;
     float h2 = dht2.readHumidity();
     float t2 = dht2.readTemperature() * 1.8 + 32;
-    Serial.println("h1 sensor value: " + String(h1));
-    Serial.println("t1 sensor value: " + String(t1));
-
-    Serial.println("h2 sensor value: " + String(h2));
-    Serial.println("t2 sensor value: " + String(t2));
-
 
     if (!isnan(h1) && !isnan(t1) && !isnan(h2) && !isnan(t2)) {
         String payload = "{\"sensor1\": {\"temp\": " + String(t1) +
@@ -80,8 +78,10 @@ void loop() {
                          "\"sensor2\": {\"temp\": " + String(t2) +
                          ", \"humidity\": " + String(h2) + "}}";
 
-        client.publish(topic, payload.c_str());
-        Serial.println("Published: " + payload);
+        client.publish(topic.c_str(), payload.c_str());
+        Serial.println("Published to topic: " + topic);
+        Serial.println("Payload: " + payload);
+
     } else {
         Serial.println("Failed to read from DHT sensors");
     }
