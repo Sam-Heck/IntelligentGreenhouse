80b4f77a062a9b7bf69c1f281491c8d0b39eab02
Sam Heck <heck.sam@gmail.com>
2025-05-13 17:08:48 -0700

    config(firmware): add custom User_Setup.h for TFT display pinout and configs.

---
+++ firmware/sensor-node/lib/TFT_eSPI/User_Setups/SetupGreenhouseDisplay.h
@@ -0,0 +1,14 @@
+#define ILI9341_DRIVER
+
+#define TFT_CS 5
+#define TFT_DC 2
+#define TFT_RST 4
+
+#define TFT_MOSI 23
+#define TFT_SCLK 18
+#define LOAD_GLCD
+#define LOAD_FONT2
+#define LOAD_FONT4
+#define SMOOTH_FONT
+
+#define SPI_FREQUENCY 27000000
